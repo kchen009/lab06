4e6659ce709a78b2dd359f932915fc85ae40f46c
Kuan <kuan@Kuans-MBP.home>
2019-04-13 23:54:51 -0400

    inital commit'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,229 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import React, { Component, useState } from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { observable, action, computed } from "mobx";
+import { observer } from "mobx-react";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Stores are where the business logic resides
+class TodoStore {
+  nextID = 0;
+  @observable todos = [];
+  @observable filterType = "all";
+
+  // compute a filtered list of todos
+  @computed
+  get filtered() {
+    if (this.filterType === "all") {
+      return this.todos;
+    } else if (this.filterType === "completed") {
+      return this.todos.filter(t => t.completed);
+    } else {
+      return this.todos.filter(t => !t.completed);
+    }
+  }
+
+  @computed
+  get totalTodoCount() {
+    return this.todos.length;
+  }
+
+  @computed
+  get completedTodoCount() {
+    let count = this.todos.filter(t => t.completed).length
+    return count;
+  }
+
+  // set a filter type: "all", "completed" or "active"
+  @action
+  setFilterType(filterType) {
+    this.filterType = filterType;
+  }
+
+  // create a todo
+  @action
+  create = name => {
+    let id = this.nextID;
+    this.nextID += 1;
+    this.todos.push({ id: id, name: name, completed: false });
+  };
+
+  // toggle the completion state of a todo
+  @action
+  toggle = id => {
+    let todo = this.todos.find(e => e.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+    }
+  };
+
+  // remove completed todos
+  @action
+  removeCompletedTodos() {
+    this.todos.replace(this.todos.filter(t => !t.completed));
+  }
+}
+
+@observer
+class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  @observable inputValue = "";
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.todoStore.create(this.inputValue);
+    this.inputValue = "";
+  };
+
+  @action
+  handleInputChange = event => {
+    this.inputValue = event.target.value;
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <label>
+          <input
+            type="text"
+            name="name"
+            value={this.inputValue}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <input type="submit" value="Create Todo" />
+      </form>
+    );
+  }
+}
+
+const Link = ({ active, children, onClick }) => {
+  if (active) {
+    return (
+      <span>
+        {" | "} {children}
+      </span>
+    );
+  }
+
+  return (
+    <a href="#" onClick={onClick}>
+      {" | "}
+      {children}
+    </a>
+  );
+};
+
+const TodoFilter = observer(({ todoStore }) => (
+  <span>
+    <b>Filter Todos</b>:
+    {["all", "completed", "active"].map((status, i) => (
+      <Link
+        key={i}
+        active={todoStore.filterType === status}
+        onClick={() => todoStore.setFilterType(status)}
+      >
+        {status}
+      </Link>
+    ))}
+  </span>
+));
+
+const TodoList = observer(({ todoStore }) => (
+  <table>
+    <tbody>
+      <tr>
+        <th>Status</th>
+        <th>Name</th>
+      </tr>
+      {todoStore.filtered.map(t => (
+        <TodoView
+          key={t.id}
+          name={t.name}
+          completed={t.completed}
+          onClick={() => {
+            todoStore.toggle(t.id);
+          }}
+        />
+      ))}
+    </tbody>
+  </table>
+));
+
+const TodoView = ({ onClick, completed, name }) => (
+  <tr >
+    <td>
+      <input type="checkbox"
+        onChange={onClick}
+        checked={completed}
+      />
+    </td>
+    <td>
+      {name}
+    </td>
+  </tr>
+);
+
+const TotalCompletedCount = observer(({ todoStore }) => (
+  <div>
+    {todoStore.completedTodoCount} tasks of {todoStore.totalTodoCount} completed
+  </div>
+));
+
+const RemoveCompletedTodos = observer(({ todoStore }) => (
+  <div>
+    <button onClick={(event) => todoStore.removeCompletedTodos()}>
+      Remove Completed Tasks
+    </button>
+  </div>
+));
+
+@observer
+class JSONView extends Component {
+  @observable showJSON = false;
+
+  toggleShowJSON = () => {
+    this.showJSON = !this.showJSON;
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="checkbox"
+          name="showjson"
+          value={this.showJSON}
+          onChange={this.toggleShowJSON}
+        />
+        Show JSON
+        {this.showJSON && <p>{JSON.stringify(this.props.store)}</p>}
+      </div>
+    );
+  }
+}
+
+const TodoApp = observer(() => {
+
+  const todoStore = new TodoStore();
+  document.todoStore = todoStore
+
+  console.log('todoapp')
+  return (
+    <div>
+      <TodoForm todoStore={todoStore} />
+      <hr />
+      <TodoFilter todoStore={todoStore} />
+      <hr />
+      <TodoList todoStore={todoStore} />
+      <TotalCompletedCount todoStore={todoStore} />
+      <RemoveCompletedTodos todoStore={todoStore} />
+      <hr />
+      <JSONView store={todoStore} />
+    </div>)
+})
+
+
+render(<TodoApp />, document.getElementById("app"));
